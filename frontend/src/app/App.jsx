25ec2a90ff3ff91137bdf1c3eda61b2f0b05206c
Mutya Mustafa <mutya2112@gmail.com>
2026-07-13 11:14:53 +0700

    added and modified login layouts
    added admin login layout
    modified user login layout

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { defaultMovie, getMovieKey } from "../features/movies/data/movies.js";
 import { useMovies } from "../features/movies/hooks/useMovies.js";
 import { useHashRoute } from "../hooks/useHashRoute.js";
 import { AdminPage } from "../pages/AdminPage.jsx";
 import { HomePage } from "../pages/HomePage.jsx";
+import LoginPage from "../pages/LoginPage.jsx";
+import AdminLoginPage from "../pages/AdminLoginPage.jsx";
+import RegisterPage from "../pages/RegisterPage.jsx";
 import { MovieDetailPage } from "../pages/MovieDetailPage.jsx";
 import { PaymentPage } from "../pages/PaymentPage.jsx";
 import { SeatSelectionPage } from "../pages/SeatSelectionPage.jsx";
@@ -14,6 +18,7 @@ function getInitialMovie(movies) {
 }
 
 export default function App() {
+  const location = useLocation();
   const route = useHashRoute();
   const { filters, movies, loading, error, pagination, setFilters } = useMovies();
   const [selectedMovie, setSelectedMovie] = useState(() => getInitialMovie(movies));
@@ -25,6 +30,20 @@ export default function App() {
   function chooseMovie(movie) {
     setSelectedMovie(movie);
     localStorage.setItem("selectedMovie", getMovieKey(movie) || movie.title);
+  }
+
+  const hashPath = location.hash ? location.hash.replace(/^#/, '') : '';
+
+  if (location.pathname === "/login" || hashPath === "/login" || hashPath === "login") {
+    return <LoginPage />;
+  }
+
+  if (location.pathname === "/admin-login" || hashPath === "/admin-login" || hashPath === "admin-login") {
+    return <AdminLoginPage />;
+  }
+
+  if (location.pathname === "/register" || hashPath === "/register" || hashPath === "register") {
+    return <RegisterPage />;
   }
 
   if (route === "booking") {
